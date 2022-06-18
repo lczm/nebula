ecfdbc351a4eb01df9dd6a499bf78d4c38f2a466
lczm <zemingchua@gmail.com>
2022-06-18 23:07:15 +0800

    Fix hashmap collision by inserting using linear probing

--- hashmap.c
+++ hashmap.c
@@ -16,10 +16,49 @@ static int resize_capacity(int capacity) {
   return capacity * 2;
 }
 
+static Entry* find_entry(Entry* entries, int capacity, ObjString* key) {
+  int index = key->hash % capacity;
+  // printf("find_entry : key : %s | hash : %d | bucket : %d | \n", key->chars,
+  //        key->hash, index);
+  // return &entries[index];
+
+  for (;;) {
+    Entry* entry = &entries[index];
+    // Check if the entry is null first
+    if (entry->key == NULL) {
+      return entry;
+    }
+
+    // Otherwise, check that the key already exists, and it is the same key
+    // Note that we cannot do entry->key == key->chars as a check here
+    // because string equality is not the same.
+    if (strncmp(entry->key->chars, key->chars, entry->key->length) == 0) {
+      return entry;
+    }
+
+    // there exists a key but it does not match the same key,
+    // then we give it the next bucket, incrementing it by 1
+    // for linear probing, modulo so that it will wrap to the
+    // beginning if it is at the end
+    index = (index + 1) % capacity;
+  }
+}
+
+static void add_all_hashmap(HashMap* from, HashMap* to) {
+  for (int i = 0; i < from->capacity; i++) {
+    Entry* entry = &from->entries[i];
+    if (entry->key != NULL) {
+      push_hashmap(to, entry->key, entry->value);
+    }
+  }
+}
+
 // This function gets called when the load factor gets hit
 static void adjust_capacity(HashMap* hashmap) {
+  // printf("adjust_capacity called\n");
   int resized_capacity = resize_capacity(hashmap->capacity);
   Entry* entries = ALLOCATE(Entry, resized_capacity);
+
   // Set all new entries to null
   for (int i = 0; i < resized_capacity; i++) {
     entries[i].key = NULL;
@@ -33,19 +72,31 @@ static void adjust_capacity(HashMap* hashmap) {
     // Ignore if empty
     if (entry->key == NULL)
       continue;
-    int hash_bucket = entry->key->hash % resized_capacity;
-    entries[hash_bucket].key = entry->key;
-    entries[hash_bucket].value = entry->value;
+
+    Entry* destination = find_entry(entries, resized_capacity, entry->key);
+    destination->key = entry->key;
+    destination->value = entry->value;
+
+    // int hash_bucket = entry->key->hash % resized_capacity;
+    // entries[hash_bucket].key = entry->key;
+    // entries[hash_bucket].value = entry->value;
+
     // Reset them just in case
-    entry->key = NULL;
-    entry->value = NIL_VAL;
+    // entry->key = NULL;
+    // entry->value = NIL_VAL;
     // Increment count
     hashmap->count++;
   }
+
+  // printf("@@@@@\n");
+  // printf("adjusted capacity | old : %d | new : %d | \n", hashmap->capacity,
+  //        resized_capacity);
+  // printf("@@@@@\n");
+
   hashmap->capacity = resized_capacity;
 
   // Free the old hashmap array
-  free(hashmap->entries);
+  // free(hashmap->entries);
   // Set the new hashmap array
   hashmap->entries = entries;
 }
@@ -59,36 +110,55 @@ void init_hashmap(HashMap* hashmap) {
 void push_hashmap(HashMap* hashmap, ObjString* key, Value value) {
   // Check if there is a need to adjust_capacity based off
   // the load factor
-  if (hashmap->count >= hashmap->capacity * LOAD_FACTOR) {
-    printf("Before adjusting capacity: %d\n", hashmap->capacity);
+  if (hashmap->count + 1 > hashmap->capacity * LOAD_FACTOR) {
+    // printf("Before adjusting capacity: %d\n", hashmap->capacity);
     adjust_capacity(hashmap);
-    printf("After adjusting capacity: %d\n", hashmap->capacity);
+    // printf("After adjusting capacity: %d\n", hashmap->capacity);
   }
 
-  int hash_bucket = key->hash % hashmap->capacity;
+  // Previous implementation that does not consider
+  // collision and linear probing
+  // int hash_bucket = key->hash % hashmap->capacity;
+  // Entry* entry = &hashmap->entries[hash_bucket];
+
+  Entry* entry = find_entry(hashmap->entries, hashmap->capacity, key);
+
+  // the entry is new if there was no previous keys
+  // bool new_key = entry->key == NULL;
 
   // Only if the hashmap entry is null
   // then the count should be incremented
-  if (hashmap->entries[hash_bucket].key == NULL)
+  if (entry->key == NULL)
     hashmap->count++;
 
   // Set the key and value in that hash_bucket
-  hashmap->entries[hash_bucket].key = key;
-  hashmap->entries[hash_bucket].value = value;
+  entry->key = key;
+  entry->value = value;
 }
 
+// TODO: Being able to free this will probably free up a lot of the
+// memory leakage issues
 void free_hashmap(HashMap* hashmap) {}
 
 Value get_hashmap(HashMap* hashmap, ObjString* key) {
   if (hashmap->count == 0)
     return NIL_VAL;
 
-  int hash_bucket = key->hash % hashmap->capacity;
-  Entry* entry = &hashmap->entries[hash_bucket];
+  // Previous implementation that did not consider collision
+  // int hash_bucket = key->hash % hashmap->capacity;
+  // Entry* entry = &hashmap->entries[hash_bucket];
+
+  Entry* entry = find_entry(hashmap->entries, hashmap->capacity, key);
+  if (entry->key == NULL)
+    return NIL_VAL;
 
   // Debugging whether the key (ObjString) gives the same (Entry) value.
-  printf("get_hashmap: %s | %d | hash_bucket: %d| entry: %p \n", key->chars,
-         key->hash, hash_bucket, entry);
+  // printf("get_hashmap: %s | %d | hash_bucket: %d| entry: %p \n", key->chars,
+  //     key->hash, hash_bucket, entry);
+
+  // Value value = entry->value;
+  // double number = AS_NUMBER(value);
+  // printf("key: %s | value number: %f\n", entry->key->chars, number);
 
   return entry->value;
 }
